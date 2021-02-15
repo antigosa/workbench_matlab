333f940c2a6d9f03def0d785811a9ffe362a29df
Raffaele <antigosa@users.noreply.github.com>
2021-02-15 15:27:40 +0000

    adapted to work for my mac
    I have just changed one line of of code in the wb_parameters.m file, so it works also in my mac. It won't work on other computers yet, it is not that generic.

--- wb_parameters.m
+++ wb_parameters.m
@@ -2,8 +2,16 @@ function wb_cfg = wb_parameters(test_wb_command)
 
 if nargin==0; test_wb_command=0;end
 
-wb_cfg.pathToWB     = 'D:\Programs\neuro\workbench\bin_windows64';
-wb_cfg.wb_command   = fullfile(wb_cfg.pathToWB, 'wb_command.exe');
+% this works only on my Desktop and Mac for now
+
+if IsWin
+    wb_cfg.pathToWB     = 'D:\Programs\neuro\workbench\bin_windows64';
+    wb_cfg.wb_command   = fullfile(wb_cfg.pathToWB, 'wb_command.exe');
+else
+    wb_cfg.pathToWB     = '/Applications/workbench/bin_macosx64';
+    wb_cfg.wb_command   = fullfile(wb_cfg.pathToWB, 'wb_command');
+end
+
 
 if test_wb_command
     system(wb_cfg.wb_command);
