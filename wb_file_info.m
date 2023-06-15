b74b0d51ca1db28ea2284095e24a5d89be4c344e
Raffaele Tucciarelli <rt03@login-j02.mrc-cbu.cam.ac.uk>
2023-06-15 20:20:16 +0100

    updated function wb_file_info.m

--- wb_file_info.m
+++ wb_file_info.m
@@ -1,25 +1,27 @@
-function [status,cmdout] = wb_file_info(fname)
-% function [status,cmdout] = wb_file_info(wb_par, surf_file)
+function [status,cmdout] = wb_file_info(fname, s)
+% function [status,cmdout] = wb_file_info(fname, s)
 %    wb_command -file-information
 %       <data-file> - data file
-% 
+%
 %       [-no-map-info] - do not show map information for files that support maps
-% 
+%
 %       [-only-step-interval] - suppress normal output, print the interval
 %          between maps
-% 
+%
 %       [-only-number-of-maps] - suppress normal output, print the number of maps
-% 
+%
 %       [-only-map-names] - suppress normal output, print the names of all maps
-% 
+%
 %       [-only-metadata] - suppress normal output, print file metadata
-% 
+%
 %          [-key] - only print the metadata for one key, with no formatting
 %             <key> - the metadata key
-% 
+%
 %       List information about the content of a data file.  Only one -only option
 %       may be specified.  The information listed when no -only option is present
 %       is dependent upon the type of data file.
+
+if nargin<2;s=[];end
 
 % =========================================================================
 % wb_command
@@ -31,6 +33,8 @@ wb_command=wb_par.wb_command;
 % command string
 % =========================================================================
 wb_cmd=[wb_command ' -file-information ' fname];
+
+if ~isempty(s); wb_cmd=[wb_cmd ' ' s]; end
 
 % =========================================================================
 % execute program
